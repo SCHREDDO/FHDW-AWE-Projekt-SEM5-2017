49592466a1c9fca276236865a9027236ee570b2d
Jannis I <jannis.isaak@gmail.com>
2017-12-12 15:46:05 +0100

    test
    test

---
+++ app/BankApplication/app/src/main/java/fhdw/bankapplication/LoginActivity.java
@@ -0,0 +1,24 @@
+package fhdw.bankapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by Jannis on 11.12.2017.
+ */
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+
+
+
+
+}
